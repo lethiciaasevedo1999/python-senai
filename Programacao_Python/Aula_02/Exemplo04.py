69bf52559493b6d6d7b827076f95fd9af9b9fc39
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-11-01 17:13:42 -0300

    Trabalhando com operadores lógicos e inputs

---
+++ Programacao_Python/Aula_02/Exemplo04.py
@@ -0,0 +1,21 @@
+#Trabalhando com operadores lógicos e inputs
+
+#Soma
+valor_01 = float(input("Digite um valor: "))
+valor_02 = float(input("Digite outro valor: "))
+
+soma = valor_01 + valor_02
+print(f"O resultado é: {soma} ")
+
+#Exponenciação
+exponenciacao = valor_01 ** valor_02
+print(f"O resultado é: {exponenciacao} ")
+
+#Divisão
+#divisao = valor_01 // valor_02 --> Para retornar a divisão inteira
+divisao = valor_01 / valor_02
+print(f"O resultado é: {divisao} ")
+
+#Resto
+resto = valor_01 % valor_02
+print(f"O resultado é: {resto} ")
