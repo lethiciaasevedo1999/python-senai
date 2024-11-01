cdf94cc9ddafa0eeebe5a3b24a7b90bd74fc5f1f
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-11-01 16:48:33 -0300

    Trabalhando com tipos de variáveis nos inputs

---
+++ Programacao_Python/Aula_02/Exemplo02.py
@@ -0,0 +1,17 @@
+#Variáveis com input
+
+#String
+variavel = input("Digite um número: ")
+print(type(variavel))
+
+#Int
+variavel = int (input("Digite um número: "))
+print(type(variavel))
+
+#Float 
+variavel = float (input("Digite um número: "))
+print(type(variavel))
+
+#Boolean
+variavel = 5 < 6 
+print(variavel)
