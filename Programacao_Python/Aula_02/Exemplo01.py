d5815f7ea48fa3147a7b8652a3b1a27daa0cc44d
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-11-01 16:21:24 -0300

    Entendendo os tipos de variáveis

---
+++ Programacao_Python/Aula_02/Exemplo01.py
@@ -0,0 +1,21 @@
+#Trabalhando com tipos de variáveis
+
+#Int
+variavel = 58
+print(variavel)
+print(type(variavel))
+
+#String
+variavel = "58"
+print(variavel)
+print(type(variavel))
+
+#Float
+variavel = 58.90
+print(variavel)
+print(type(variavel))
+
+#Números complexos
+variavel = 3 + 4j
+print(variavel)
+print(type(variavel))
