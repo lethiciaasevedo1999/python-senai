b56e668995ceb5407b3df1c3967747cc855a61fb
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-11-05 14:23:07 -0300

    Mostrando exemplos de operadores

---
+++ Programacao_Python/Aula_03/Exemplo01.py
@@ -0,0 +1,45 @@
+#Trabalhando com operadores 
+
+''' 
+Operadores de comparação:
+    menor (<)
+    igual (==)
+    diferente (!=)
+    maior igual (>=)
+    menor igual (<=)
+
+Operadores aritméticos: 
+    Soma  (+)
+    Subtração (-)
+    Multiplicação (*)
+    Divisão (/)
+    Divisão inteira (//)
+    Exponenciação (**)
+    Resto mod (%)
+
+Operadores lógicos:
+    E = AND
+    OU = OR
+    NÃO = NOT 
+             '''
+
+valor_01 = 357
+valor_02 = 2
+#soma
+total = valor_01 + valor_02
+print(total)
+#subtração
+total = valor_01 - valor_02
+print(total)
+#multiplicação
+total = valor_01 * valor_02
+print(total)
+#divisão
+total = valor_01 / valor_02
+print(total)
+#divisão total
+total = valor_01 // valor_02
+print(total)
+#mod
+total = valor_01 % valor_02
+print(total)
