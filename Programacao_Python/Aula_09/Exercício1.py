be3ab75132e81b58f231e7df5a1796f93bd7e5b6
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-11-14 17:19:20 -0300

    Exercícios para fazer

---
+++ Programacao_Python/Aula_09/Exercício1.py
@@ -0,0 +1,9 @@
+'''1. Contar o número de caracteres em uma String
+2. Verificar se uma substring está presente em uma String
+3. Converter uma string para maiúsculas
+4. Converter uma string para minúsculas
+5. Remover espaços extras no início e no final de uma String
+6. Substituir uma substring em uma String
+8. Inverter uma String
+9. Verificar se uma string é um palíndromo
+'''
